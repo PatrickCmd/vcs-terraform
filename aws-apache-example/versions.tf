2843817bd4f480634b86c81a77caa445002ced08
Walukagga Patrick <pwalukagga@gmail.com>
2023-11-29 19:06:24 +0000

    Add Cloud environment

--- aws-apache-example/versions.tf
+++ aws-apache-example/versions.tf
@@ -1,9 +1,18 @@
 terraform {
-  required_version = ">= 1.6.4"
+  cloud {
+    organization = "PatrickCmdCloud"
+
+    workspaces {
+      name = "terraform-vcs"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.28.0"
     }
   }
+
+  required_version = ">= 1.6.0"
 }
